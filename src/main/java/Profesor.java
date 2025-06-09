2082a7d32dcef1338ed0a7a7ea5feac5affdfb60
Alejo <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-06-09 14:03:52 -0500

    1b) Se crea clase hija "Profesor", heredando atributos de Persona con palabra clave EXTENDS, concepto de profesor (objeto hija) hereda de persona (clase Padre). Docs de codigo general.

---
+++ src/main/java/Profesor.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList; /* Libreria de java, Permite usar listas dinámicas en Java. */
+
+/* PUNTO 1b) CLASE PROFESOR: CREACION Y CONSTRUCCION CLASE HIJA: representa un profesor, heredando atributos de Persona. */
+public class Profesor extends Persona { //Aqui con palabra clave EXTENDS profesor hereda de persona
+    private String especialidad; //Se agrega atributo nuevo
+    private String nivelAcademico; // se agrega atributo nuevo
+
+}
+
