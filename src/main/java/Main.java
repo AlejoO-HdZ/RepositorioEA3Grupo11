b801e748117315b298990d6179a0f9fc4ce15592
Alejo <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-06-09 15:01:26 -0500

    Se crea el MAIN del proyecto, con estructira basica, se ajustan y validan metodos y constructores en clase Padre y clase Hija.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner; /* Libreria de java, Permite leer datos desde la consola.*/
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
