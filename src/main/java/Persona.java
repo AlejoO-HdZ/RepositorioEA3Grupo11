5936349f0e549f5da8297ee4ee67d9b244d5051f
Alejo <211151669+AlejoO-HdZ@users.noreply.github.com>
2025-06-09 13:55:05 -0500

    1a) Se crea la clase publica Padre Persona con los atributos basicos respectivos con acceso private.

---
+++ src/main/java/Persona.java
@@ -0,0 +1,11 @@
+/* PUNTO 1a) CLASE PERSONA: CREACION Y CONSTRUCCION DE CLASE PADRE representa una persona con atributos básicos.*/
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String fechaNacimiento;
+    private String genero;
+    private double estatura;
+    private double peso;
+
+
+}
